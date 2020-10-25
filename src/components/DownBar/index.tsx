c693e71dc950059f36aae8572051842eebfdfc87
marcelog5 <marcelovictorg2@gmail.com>
2020-10-25 08:56:05 -0300

    Create DownBar component

---
+++ src/components/DownBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Footer } from './styles';
+
+const DownBar: React.FC = () => (
+  <Footer>
+    <p>Foto do freepick</p>
+  </Footer>
+);
+
+export default DownBar;
---
+++ src/components/DownBar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Footer = styled.div`
+  width: 100%;
+  height: 40px;
+  color: white;
+  background-color: black;
+`;
