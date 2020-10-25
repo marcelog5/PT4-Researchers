55e69e7132eb38715bf6c86b5d04d64399cef66c
marcelog5 <marcelovictorg2@gmail.com>
2020-10-25 08:56:20 -0300

    Create UpBar component

---
+++ src/components/UpBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Header, Logo, Bar } from './styles';
+
+const UpBar: React.FC = () => (
+  <Header>
+    <Logo>
+      <h1>Logo</h1>
+    </Logo>
+    <Bar></Bar>
+  </Header>
+);
+
+export default UpBar;
---
+++ src/components/UpBar/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  height: 60px;
+`;
+
+export const Logo = styled.div`
+  width: 10%;
+  background-color: #000;
+  color: white;
+`;
+
+export const Bar = styled.div`
+  width: 90%;
+  background-color: #c4f1f2;
+`
