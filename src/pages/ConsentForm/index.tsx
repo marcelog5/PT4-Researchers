e8b2dbab086f363496db565c64de1fa533957042
marcelog5 <marcelovictorg2@gmail.com>
2020-10-26 09:44:41 -0300

    Install axios and Create ConsentForm page

--- src/pages/ConsentForm/index.tsx
+++ src/pages/ConsentForm/index.tsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const ConsentForm: React.FC = () => (
-  <>
-    <h1>Ola voce esta ligado</h1>
-  </>
-);
+import api from '../../services/api';
+
+import UpBar from '../../components/UpBar';
+import DownBar from '../../components/DownBar';
+import ButtonDefault from '../../components/ButtonDefault';
+
+import { Background, Container, ResearchLink, ResearchTerm, ContainerButton } from './styles';
+
+
+interface Question {
+  question: string;
+  inverted: boolean;
+  trait: string;
+  factor: string;
+}
+
+interface Inventory {
+  id: string;
+  author: string;
+  numberOfQuestions: number;
+  question: Question[];
+}
+
+interface Form {
+  id: string;
+  name: string;
+  term: string;
+  link: string;
+  inventory: Inventory;
+}
+
+const ConsentForm: React.FC = () => {
+  const [forms, setForms] = useState<Form>();
+
+  useEffect(() => {
+    api.get('forms/46582820-ecc6-4530-bff6-ce3c44167e81').then(response => {
+      setForms(response.data);
+    });
+  }, []);
+
+
+
+  return (
+    <>
+      <UpBar />
+
+      <Background>
+        <Container>
+          <ResearchLink>
+            <p>Link para o Termo de consentimento do pesquisador: <a href={forms?.link}>{forms?.link}</a></p>
+          </ResearchLink>
+
+          <ResearchTerm>
+            <p>Termo de consentimento escrito: </p>
+            <p>{forms?.term}</p>
+          </ResearchTerm>
+
+          <ContainerButton>
+            <Link to="/">
+              <ButtonDefault type="button">
+                Cancelar
+              </ButtonDefault>
+            </Link>
+
+            <Link to="/questionsform">
+              <ButtonDefault type="button">
+                Aceito
+              </ButtonDefault>
+            </Link>
+          </ContainerButton>
+        </Container>
+      </Background>
+
+      <DownBar />
+    </>
+  );
+};
 
 export default ConsentForm;
--- src/pages/ConsentForm/styles.ts
+++ src/pages/ConsentForm/styles.ts
@@ -1,1 +1,40 @@
 import styled from 'styled-components';
+
+import BackgroundImg from '../../assets/BackgroundBase.jpg';
+
+export const Background = styled.div`
+  background-image: url(${BackgroundImg});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  background-color: rgba(235, 235, 235, 0.8);
+  position: absolute;
+  top: 47%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50px;
+`
+
+export const ResearchLink = styled.div`
+  margin: 40px 2%;
+`;
+
+export const ResearchTerm = styled.div`
+  margin: 40px 2%;
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  display: 1;
+  justify-content: space-between;
+  margin: 40px 2%;
+`;
